f091708cf7ccb8ab12135a07defca9ebe2627a49
Kuldeep Singh Rai <39896367+imkuldeepsinghrai@users.noreply.github.com>
2020-12-03 03:38:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
-import random
-randNumber = random.randint(1, 100)
+import random 
+
+randNumber = random.randint(1, 100) #--> generating random number 1 to 100
 userGuess = None
 guesses = 0
 
-#user will input the number
+
 while(userGuess != randNumber):
     userGuess = int(input("Enter your guess: "))
     guesses += 1
 	
+    #when user guesses it
     if(userGuess==randNumber):
         print("You guessed it right!")
 
     else:
+	#if user enters higher number
         if(userGuess>randNumber):
             print("You guessed it wrong! Enter a Lower number")
+	#if user enters lower number
         else:
             print("You guessed it wrong! Enter a Higher number")
 
+#how much attempts the user made to guess correct number	
 print(f"You guessed the number in {guesses} guesses")
 with open("hiscore.txt", "r") as f:
     hiscore = int(f.read())
-
+#user breaks the highsocore which is in highscor.txt
 if(guesses<hiscore):
     print("You have just broken the high score!")
     with open("hiscore.txt", "w") as f:
